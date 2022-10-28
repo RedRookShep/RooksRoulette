5c5681cfa41c9ab6b56a278f673e90fd6db93dbc
Michael Paulsen <mpaulsen@cds.local>
2022-10-28 16:32:50 -0400

    Friend modes, styling fixes, tag removal, new icons.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, HostListener, ViewChild } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 import { catchError, map, tap } from 'rxjs/operators';
 import { ApiData, Post } from './models/post.model';
@@ -11,17 +11,37 @@ import { MatSidenav } from '@angular/material/sidenav';
 })
 export class AppComponent {
 
+	@ViewChild('drawer') drawer!: MatSidenav;
+	
+	@HostListener('document:keydown', ['$event'])
+	handleKeyboardEvent(event: KeyboardEvent) {
+		if (event.code == 'ArrowLeft') {
+			this.back();
+		}
+		if (event.code == 'ArrowRight') {
+			this.spinWheel();
+		}
+		if (event.code == 'Tab') {
+			event.preventDefault();
+			this.drawer.toggle();
+		}
+	}
+  
 	private apiUrl: string = 'https://e621.net/posts.json';
 	private login: string = 'RedRookShep';
 	private apiKey: string = 'qCANh8LhWtMDa7ytSjKYes6m';
-	private postLimit: number = 10;//320;
+	private postLimit: number = 320;
 	private clientString: string = 'twitter_redrookshep';
 	public tags: string[] = [];
 	public posts: Post[] = [];
-	public postCount: number = 0;
 	public currentPost: number = -1;
 	public random: boolean = true;
 	public errorText: string = "";
+	
+	public bayleMode: boolean = false;
+	public briarMode: boolean = false;
+	public mysteryMode: boolean = false;
+	public vulcanMode: boolean = false;
   
 	constructor(private http: HttpClient) { };
 	
@@ -32,9 +52,9 @@ export class AppComponent {
 			this.currentPost = -1;
 			this.getPosts(true);
 		}
-		else if (this.currentPost < this.postCount - 1) {
+		else if (this.currentPost < this.posts.length - 1) {
 			this.currentPost++;
-			if (this.currentPost == this.postCount - 5)
+			if (this.currentPost == this.posts.length - 5)
 				this.getPosts(false);
 		}
 		else {
@@ -49,10 +69,40 @@ export class AppComponent {
   
 	public getPosts(stepOnFetch: boolean) {
 		console.log("Getting Posts");
-		var tags = [...this.tags];
+		var tags: string[] = [];
+		if (this.bayleMode) {
+			tags.push("rating:safe");
+			tags.push("cub");
+			tags.push("male");
+			tags.push("clothing");
+			tags.push("-pregnant");
+			tags.push("-absolutely_everyone");
+			tags.push("score:>99");
+		}
+		if (this.briarMode) {
+			tags.push("~border_collie");
+			tags.push("~harness");
+			tags.push("canine");
+			tags.push("score:>99");
+		}
+		if (this.mysteryMode) {
+			tags.push('score:>99');
+			tags.push('doughnut');
+		}
+		if (this.vulcanMode) {
+			tags.push('~rubble_(paw_patrol)');
+			tags.push('~violin');
+		}
+		//Only if no friend modes are active, use the entered tags.; 
+		if (tags.length < 1) {
+			tags = [...this.tags];
+		}
 		if (this.random)
 			tags.push("order:random");
+		
+		//Don't suppport flash, sorry.
 		tags.push("-flash");
+		tags.push("-scat");
 
 		var queryParams = new HttpParams()
 			.set('limit', this.postLimit)
@@ -64,11 +114,18 @@ export class AppComponent {
 
 		this.http.get<ApiData>(this.apiUrl, {params: queryParams})
 			.subscribe(data => {
-				console.log(data);
-				this.posts = [...this.posts, ...data.posts];
-				this.postCount = this.posts.length;
-				if (stepOnFetch)
-					this.currentPost++;
+				if (data.posts.length < 1) {
+					this.errorText = "No Posts Found";
+					this.posts = [];
+					this.currentPost = -1;
+				}
+				else {
+					this.posts = [...this.posts, ...data.posts];
+					this.posts.length = this.posts.length;
+					if (stepOnFetch)
+						this.currentPost++;
+				}
+				
 			});
 	}
 	
@@ -96,12 +153,26 @@ export class AppComponent {
 		window.open("https://e621.net/posts/" + this.posts[this.currentPost]?.id, '_blank');
 	}
 	
-	public addTag($event: any) {
-		var tag: string = $event.value.toLowerCase();
-		if (!tag) return;
-		if (this.tags.indexOf(tag) < 0)
-			this.tags.push(tag);
+	public addTagSubmit($event: any) {
+		var tag: string = $event.value;
+		this.addTag(tag);
 		$event.value = '';
+	}
+	
+	public addTag(tag: string) {
+		if (!tag) return;
+		if (this.tags.indexOf(tag) < 0) {
+			this.tags.push(tag);
+			this.flushCache();
+		}
+	}
+	
+	public removeTag(tag: string) {
+		this.tags = this.tags.filter(t => t != tag);
+	}
+	
+	public flushCache() {
+		this.posts.splice(this.currentPost + 1);
 	}
 	
 }
